18032db011d195aff41264f2480cc8c604a29a78
zxymiku <95949338+zxymiku@users.noreply.github.com>
2024-07-20 19:53:54 +0800

    change

--- dis.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'D:\Tesseract-OCR\tesseract.exe'
-image = Image.open(r'C:\Users\Divin\Desktop\weici\test.png')
-text = pytesseract.image_to_string(image, lang='eng')
-print(text)
